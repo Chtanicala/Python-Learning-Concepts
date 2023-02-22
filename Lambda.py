7e9223f1957284ff2cdcbb9be835f1f893a23528
Christian Tanicala <christian.tanicala@gmail.com>
2023-02-21 22:34:00 -0500

    final lambda:

--- Lambda.py
+++ Lambda.py
@@ -2,10 +2,11 @@ double = lambda x:x*2
 multiply = lambda x,y:x*y
 add = lambda x,y,z:x+y+z
 full_name = lambda first_name, last_name: first_name + " " +last_name
+age_check = lambda age:True if age >= 18 else False
 
 print(double(2))
 print(multiply(3,2))
-
 print(add(3,2,5))
 
 print(full_name("Christian","Tanicala"))
+print(age_check(14))
