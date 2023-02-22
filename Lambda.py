3d9e8baa3451e385517486b547275b70e1684741
Christian Tanicala <christian.tanicala@gmail.com>
2023-02-21 22:33:09 -0500

    Lambda

---
+++ Lambda.py
@@ -0,0 +1,11 @@
+double = lambda x:x*2
+multiply = lambda x,y:x*y
+add = lambda x,y,z:x+y+z
+full_name = lambda first_name, last_name: first_name + " " +last_name
+
+print(double(2))
+print(multiply(3,2))
+
+print(add(3,2,5))
+
+print(full_name("Christian","Tanicala"))
