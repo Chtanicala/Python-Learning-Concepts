82847ae1a74047a256e7b453180b248aa5cc6b50
Christian Tanicala <christian.tanicala@gmail.com>
2023-02-23 23:59:39 -0500

    With functions

--- Dictionary_Comprehensions.py
+++ Dictionary_Comprehensions.py
@@ -13,3 +13,14 @@ print(grades_in_percent)
 grades_in_percent = {key: ((value/50*100) if value > 40 else "Failed") for (key,value) in boys_grades.items()}
 
 print(grades_in_percent)
+
+def grade_conversion(value):
+    if value > 40:
+        return value/50*100
+    else:
+        return "Failed"
+    
+
+grades_in_percent = {key: (grade_conversion(value)) for (key,value) in boys_grades.items()}
+
+print(grades_in_percent)
