7abd60ea842fce7fb85e7bec18a7197d017e97c0
Christian Tanicala <christian.tanicala@gmail.com>
2023-02-23 23:54:45 -0500

    Dictionary Comprehensions

---
+++ Dictionary_Comprehensions.py
@@ -0,0 +1,11 @@
+boys_grades = {"Christian": 50, 
+                    "Amun": 45,
+                    "Michael": 40}
+
+grades_in_percent = {key: (value/50*100) for (key,value) in boys_grades.items()}
+
+print(grades_in_percent)
+
+grades_in_percent = {key: (value/50*100) for (key,value) in boys_grades.items() if value > 40}
+
+print(grades_in_percent)
