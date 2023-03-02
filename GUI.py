882de42ba638a65eea637f062a538d6694ab7138
Christian Tanicala <christian.tanicala@gmail.com>
2023-03-01 23:14:24 -0500

    Gui

---
+++ GUI.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import os
+
+window = Tk() #insantitate an instance of a window
+window.geometry("420x420")
+window.title("First GUI Program")
+
+window.config(background="#043829")
+
+window.mainloop() #place window on computer screen, listen for events
+
+
